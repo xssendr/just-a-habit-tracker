82f95276e511e9867f1a3d75dca65f7ba454b885
xssendr <afazahar171207@gmail.com>
2026-03-25 15:13:44 +0400

    pampam

--- app/root.tsx
+++ app/root.tsx
@@ -38,6 +38,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="8ZK2sFdYAeJPKE3nk86qUfME7p9ftZHiTwHxAkzQ6-0" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
